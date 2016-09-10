66718e335a0a646a0dcf56c4869f4251c759075d
Sparticle999 <alexscruton@gmail.com>
2016-09-10 21:41:45 +0100

    Added new resources

--- main.js
+++ main.js
@@ -1,24 +1,54 @@
-var stone = 0;
-var wood = 0;
-var metal = 0;
+var gas = 0; var gasGain = 1;
+var oil = 0; var oilGain = 1;
+var metal = 0; var metalGain = 1;
+var stone = 0; var stoneGain = 1;
+var wood = 0; var woodGain = 1;
 
 function refresh(){
-	document.getElementById("wood").innerHTML = wood;
-	document.getElementById("stone").innerHTML = stone;
+	document.getElementById("gas").innerHTML = gas;
+	document.getElementById("oil").innerHTML = oil;
 	document.getElementById("metal").innerHTML = metal;
+	document.getElementById("stone").innerHTML = stone;
+	document.getElementById("wood").innerHTML = wood;
 }
 
-function gainWood(){
-	wood += 1;
+function gainGas(){
+	gas += gasGain;
 	refresh();
 }
 
-function gainStone(){
-	stone += 1;
+function gainOil(){
+	oil += oilGain;
 	refresh();
 }
 
 function gainMetal(){
-	metal += 1;
+	metal += metalGain;
 	refresh();
+}
+
+function gainStone(){
+	stone += stoneGain;
+	refresh();
+}
+
+function gainWood(){
+	wood += woodGain;
+	refresh();
+}
+
+function unlockGas(){
+	gasUnlock = true;
+	document.getElementById("unlockGas").className = "hidden";
+	document.getElementById("gasNav").className = "";
+	document.getElementById("oilNav1").style.border = "";
+	document.getElementById("oilNav2").style.border = "";
+}
+
+function unlockOil(){
+	oilUnlock = true;
+	document.getElementById("unlockOil").className = "hidden";
+	document.getElementById("oilNav").className = "";
+	document.getElementById("metalNav1").style.border = "";
+	document.getElementById("metalNav2").style.border = "";
 }
