a0b3ed47d820ce92700463c1757d39a4c757f817
Sparticle999 <alexscruton@gmail.com>
2016-09-11 18:02:06 +0100

    Added passive gain

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var gas = 0; var gasStorage = 50; var gasGain = 1;
 var oil = 0; var oilStorage = 50; var oilGain = 1;
-var metal = 0; var metalStorage = 50; var metalGain = 1;
-var stone = 0; var stoneStorage = 50; var stoneGain = 1;
-var wood = 0; var woodStorage = 50; var woodGain = 1;
+var metal = 0; var metalStorage = 50; var metalGain = 1; var metalps = 0;
+	var miner = 0; var minerGain = 1;
+var stone = 0; var stoneStorage = 50; var stoneGain = 1; var stoneps = 0;
+	var digger = 0; var diggerGain = 1;
+var wood = 0; var woodStorage = 50; var woodGain = 1; var woodps = 0;
+	var woodcutter = 0; var woodcutterGain = 1;
 var science = 0; var scienceGain = 1;
 
 function refresh(){
@@ -13,6 +16,38 @@ function refresh(){
 	document.getElementById("wood").innerHTML = wood;
 	document.getElementById("science").innerHTML = science;
 }
+
+function refreshPerSec(){
+	metalps = (miner * minerGain)+(0 * 0)+(0 * 0);
+	stoneps = (digger * diggerGain)+(0 * 0)+(0 * 0);
+	woodps = (woodcutter * woodcutterGain)+(0 * 0)+(0 * 0);
+	document.getElementById("metalps").innerHTML = metalps;
+	document.getElementById("stoneps").innerHTML = stoneps;
+	document.getElementById("woodps").innerHTML = woodps;
+}
+
+function gainResources(){
+	if(metal + metalps < metalStorage){
+		metal += metalps;
+	}
+	else{
+		metal = metalStorage;
+	}
+	if(stone + stoneps < stoneStorage){
+		stone += stoneps;
+	}
+	else{
+		stone = stoneStorage;
+	}
+	if(wood + woodps < woodStorage){
+		wood += woodps;
+	}
+	else{
+		wood = woodStorage;
+	}
+}
+
+// Gain Buttons
 
 function gainGas(){
 	if(gas < gasStorage){
@@ -53,6 +88,31 @@ function gainScience(){
 	science += scienceGain;
 	refresh();
 }
+
+// Crafting Tab
+
+function getMiner(){
+	miner += 1;
+	document.getElementById("miner").innerHTML = miner;
+	refresh();
+	refreshPerSec();
+}
+
+function getDigger(){
+	digger += 1;
+	document.getElementById("digger").innerHTML = digger;
+	refresh();
+	refreshPerSec();
+}
+
+function getWoodcutter(){
+	woodcutter += 1;
+	document.getElementById("woodcutter").innerHTML = woodcutter;
+	refresh();
+	refreshPerSec();
+}
+
+// Research Tab
 
 function buildLab(){
 	if(science >= 20){
@@ -102,3 +162,8 @@ function unlockOil(){
 		alert("Not enough Science");
 	}
 }
+
+window.setInterval(function(){
+	gainResources();
+	refresh();
+},1000)
