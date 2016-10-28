3504d4c3d717e4e042a29f91446faae0c9206ec4
Sparticle999 <alexscruton@gmail.com>
2016-10-28 21:00:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-var energy = 0; var energyStorage = 50; var energyNextStorage = 100; var energyStorageCost = 50; var energyGain = 1; var energyps = 0;
-var charcoalEngine = 0; var charcoalEngineGain = 1;
+var energy = 0; var energyGain = 1; var energyps = 0;
+var charcoalEngine = 0; var charcoalEngineGain = 1; var solarPanel = 0; var solarPanelGain = 0.5;
 var oil = 0; var oilStorage = 50; var oilNextStorage = 100; var oilStorageCost = 50; var oilGain = 1; var oilps = 0;
-var pump = 0; var pumpGain = 1;
+var pump = 0; var pumpjack = 0;
 var metal = 0; var metalStorage = 50; var metalNextStorage = 100; var metalStorageCost = 50; var metalGain = 1; var metalps = 0;
-var miner = 0; var minerGain = 1;
+var miner = 0; var heavyDrill = 0;
 var gem = 0; var gemStorage = 50; var gemNextStorage = 100; var gemStorageCost = 50; var gemGain = 1; var gemps = 0;
-var extractor = 0; var extractorGain = 1;
+var gemMiner = 0; var advancedDrill = 0;
 var charcoal = 0; var charcoalStorage = 50; var charcoalNextStorage = 100; var charcoalStorageCost = 100; var charcoalGain = 1; var charcoalps = 0;
-var woodburner = 0; var woodburnerGain = 1;
+var woodburner = 0; var furnace = 0;
 var wood = 0; var woodStorage = 50; var woodNextStorage = 100; var woodStorageCost = 50; var woodGain = 1; var woodps = 0;
-var woodcutter = 0; var woodcutterGain = 1;
+var woodcutter = 0; var laserCutter = 0;
 var science = 0; var scienceps = 0;
 var lab = 0; var labGain = 0.1; var labWoodCost = 10; var labGemCost = 15; var labMetalCost = 20;
 
@@ -24,32 +24,28 @@ function refresh(){
 }
 
 function refreshPerSec(){
-	energyps = (charcoalEngine * charcoalEngineGain);
-	oilps = (pump * pumpGain);
-	metalps = (miner * minerGain);
-	gemps = (extractor * extractorGain);
-	charcoalps = (woodburner * woodburnerGain);
-	woodps = (woodcutter * woodcutterGain);
+	energyps = (charcoalEngine * charcoalEngineGain) + (solarPanel * solarPanelGain);
+	oilps = pump + (pumpjack * 5);
+	metalps = miner + (heavyDrill * 8);
+	gemps = gemMiner + (advancedDrill * 4);
+	charcoalps = woodburner + (furnace * 3);
+	woodps = woodcutter + (laserCutter * 6);
 	scienceps = (lab * labGain);
 	document.getElementById("energyps").innerHTML = energyps;
 	document.getElementById("oilps").innerHTML = oilps;
 	document.getElementById("metalps").innerHTML = metalps;
 	document.getElementById("gemps").innerHTML = gemps;
-	document.getElementById("charcoalps").innerHTML = charcoalps;
+	document.getElementById("charcoalps").innerHTML = charcoalps - charcoalEngine;
 	document.getElementById("woodps").innerHTML = woodps;
 }
 
 function gainResources(){
-	if(energy + energyps < energyStorage && charcoal + charcoalps >= energyps){
+	if(charcoal + charcoalps >= charcoalEngine){
 		energy += energyps;
-		charcoal -= energyps;
+		charcoal -= charcoalEngine;
 	}
 	else{
-		var difference = energyStorage - energy;
-		if(charcoal >= difference){
-			energy += difference;
-			charcoal -= difference;
-		}	
+		energy += solarPanel * solarPanelGain;
 	}
 	if(oil + oilps < oilStorage){
 		oil += oilps;
@@ -71,13 +67,13 @@ function gainResources(){
 	}
 	if(charcoal + charcoalps < charcoalStorage && wood + woodps >= charcoalps*2){
 		charcoal += charcoalps;
-		wood -= (charcoalps*2)
+		wood -= (charcoalps*2);
 	}
 	else{
 		var difference = charcoalStorage - charcoal;
 		if(wood >= difference*2){
 			charcoal += difference;
-			wood -= difference*2
+			wood -= difference*2;
 		}	
 	}
 	if(wood + woodps < woodStorage){
@@ -126,11 +122,6 @@ function gainWood(){
 		wood += woodGain;
 		refresh();
 	}
-}
-
-function gainScience(){
-	science += scienceGain;
-	refresh();
 }
 
 // Resources Tab
@@ -198,12 +189,35 @@ function getCharcoalEngine(){
 	}
 }
 
+function getSolarPanel(){
+	if(metal >= 30 && gem >= 35){
+		metal -= 30;
+		gem -= 35;
+		solarPanel += 1;
+		document.getElementById("solarPanel").innerHTML = solarPanel;
+		refresh();
+		refreshPerSec();
+	}
+}
+
 function getPump(){
 	if(metal >= 30 && gem >= 10){
 		metal -= 30;
 		gem -= 10;
 		pump += 1;
 		document.getElementById("pump").innerHTML = pump;
+		refresh();
+		refreshPerSec();
+	}
+}
+
+function getPumpjack(){
+	if(metal >= 250 && gem >= 80 && oil >= 50){
+		metal -= 250;
+		gem -= 80;
+		oil -= 50;
+		pumpjack += 1;
+		document.getElementById("pumpjack").innerHTML = pumpjack;
 		refresh();
 		refreshPerSec();
 	}
@@ -220,11 +234,36 @@ function getMiner(){
 	}
 }
 
-function getExtractor(){
-	if(metal >= 30){
-		metal -= 30;
-		extractor += 1;
-		document.getElementById("extractor").innerHTML = extractor;
+function getHeavyDrill(){
+	if(metal >= 160 && gem >= 60 && oil >= 50){
+		metal -= 160;
+		gem -= 60;
+		oil -= 50;
+		heavyDrill += 1;
+		document.getElementById("heavyDrill").innerHTML = heavyDrill;
+		refresh();
+		refreshPerSec();
+	}
+}
+
+function getGemMiner(){
+	if(metal >= 15 && gem >= 10){
+		metal -= 15;
+		gem -= 10;
+		gemMiner += 1;
+		document.getElementById("gemMiner").innerHTML = gemMiner;
+		refresh();
+		refreshPerSec();
+	}
+}
+
+function getAdvancedDrill(){
+	if(metal >= 120 && gem >= 200 && oil >= 60){
+		metal -= 120;
+		gem -= 200;
+		oil -= 60;
+		advancedDrill += 1;
+		document.getElementById("advancedDrill").innerHTML = advancedDrill;
 		refresh();
 		refreshPerSec();
 	}
@@ -241,12 +280,36 @@ function getWoodburner(){
 	}
 }
 
+function getFurnace(){
+	if(metal >= 80 && wood >= 40 && oil >= 100){
+		metal -= 80;
+		wood -= 40;
+		oil -= 100;
+		furnace += 1;
+		document.getElementById("furnace").innerHTML = furnace;
+		refresh();
+		refreshPerSec();
+	}
+}
+
 function getWoodcutter(){
 	if(metal >= 10 && wood >= 5){
 		metal -= 10;
 		wood -= 5;
 		woodcutter += 1;
 		document.getElementById("woodcutter").innerHTML = woodcutter;
+		refresh();
+		refreshPerSec();
+	}
+}
+
+function getLaserCutter(){
+	if(metal >= 50 && gem >= 90 && oil >= 40){
+		metal -= 50;
+		gem -= 90;
+		oil -= 40;
+		laserCutter += 1;
+		document.getElementById("laserCutter").innerHTML = laserCutter;
 		refresh();
 		refreshPerSec();
 	}
@@ -272,25 +335,14 @@ function buildLab(){
 }
 
 
-function unlockOil(){
-	if(science >= 15){
-		science -= 15;
-		document.getElementById("unlockOil").className = "hidden";
-		document.getElementById("oilNav").className = "";
-		document.getElementById("metalNav1").style.border = "";
-		document.getElementById("metalNav2").style.border = "";
-		document.getElementById("metalNav3").style.border = "";
-		refresh();
-	}
-}
-
 function unlockStorage(){
 	if(science >= 5){
 		science -= 5;
 		document.getElementById("unlockStorage").className = "hidden";
-	//	document.getElementById("oilStorageUpgrade").className = "";
+		document.getElementById("oilStorageUpgrade").className = "";
 		document.getElementById("metalStorageUpgrade").className = "";
 		document.getElementById("gemStorageUpgrade").className = "";
+		document.getElementById("charcoalStorageUpgrade").className = "";
 		document.getElementById("woodStorageUpgrade").className = "";
 	}
 }
@@ -307,12 +359,45 @@ function unlockBasicEnergy(){
 		document.getElementById("oilNav2").style.border = "";
 		document.getElementById("oilNav3").style.border = "";
 		document.getElementById("unlockBasicEnergy").className = "hidden";
+		document.getElementById("unlockOil").className = "";
+		document.getElementById("unlockSolar").className = "";
 	}
 }
 
-//Space Tech Tree Tab
+function unlockOil(){
+	if(science >= 15){
+		science -= 15;
+		document.getElementById("unlockOil").className = "hidden";
+		document.getElementById("oilNav").className = "";
+		document.getElementById("metalNav1").style.border = "";
+		document.getElementById("metalNav2").style.border = "";
+		document.getElementById("metalNav3").style.border = "";
+		refresh();
+	}
+}
+
+function unlockSolar(){
+	if(science >= 15){
+		science -= 15;
+		document.getElementById("unlockSolar").className = "hidden";
+		document.getElementById("solarPower").className = "";
+		document.getElementById("unlockMachines").className = "";
+	}
+}
+
+function unlockMachines(){
+	if(science >= 20){
+		science -= 20;
+		document.getElementById("unlockMachines").className = "hidden";
+		document.getElementById("metalMachine1").className = "";
+		document.getElementById("gemMachine1").className = "";
+		document.getElementById("woodMachine1").className = "";
+		document.getElementById("charcoalMachine1").className = "";
+		document.getElementById("oilMachine1").className = "";
+	}
+}
 
 window.setInterval(function(){
 	gainResources();
 	refresh();
-},1000)
+},1000);
