0562b63149357e7b78b0b1e08d1b27986a3d6fc0
Sparticle999 <alexscruton@gmail.com>
2017-05-06 13:43:32 +0100

    Updated FAQ

--- solarSystem.js
+++ solarSystem.js
@@ -24,7 +24,7 @@ function getRocket(){
 		gem -= 900;
 		oil -= 1000;
 		rocket = 1;
-		document.getElementById("rocket").innerHTML = rocket;
+		document.getElementById("rocket").innerHTML = "Built";
 		refresh();
 	}
 }
