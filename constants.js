ab6b3aa6b85dcabcf324b0fb8bc5fd93d4a2bee4
PatrickG <patrickg@nvizzio.com>
2017-05-11 11:19:39 -0400

    tweaked brackets to make the achievements not as easy to complete

--- constants.js
+++ constants.js
@@ -5,9 +5,9 @@ Game.constants = (function(){
     instance.iconExtension = "png";
 
     instance.achievementMax = 1000;
-    instance.achievementIconsPerRow = 5;
-    instance.achievementResourceBrackets = [10, 100, 1000, 10000, 100000];
-    instance.achievementProducerBrackets = [1, 10, 100, 500, 1000];
+    instance.achievementIconsPerRow = 4;
+    instance.achievementResourceBrackets = [500, 5000, 50000, 500000, 5000000];
+    instance.achievementProducerBrackets = [10, 50, 100, 500, 1000];
     instance.achievementBracketColors = ["#9d9d9d", "#1eff00", "#0070dd", "#a335ee", "#ff8000"];
 
     instance.achievementCategoryResources = "Resources";
