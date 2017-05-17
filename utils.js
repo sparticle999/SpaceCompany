5f52b8abb0e66d6f122b909a10f0bd9b80ab0963
Sparticle999 <alexscruton@gmail.com>
2017-05-17 22:49:59 +0100

    Tiny Change - Stats: "d" -> "Days"

--- utils.js
+++ utils.js
@@ -37,7 +37,7 @@ Game.utils = (function(){
         var hourMinutePart = this.getTimeDisplay(seconds, use24hourTime);
 
         if(timeSplit[1] > 0) {
-            return timeSplit[1] + 'd ' + hourMinutePart;
+            return timeSplit[1] + ' Days ' + hourMinutePart;
         }
 
         return hourMinutePart;
