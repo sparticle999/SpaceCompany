5792008d4161ee9ab06c1863349cd876ef817993
Ian Golsby <iangolsby49@gmail.com>
2017-07-06 14:35:18 -0500

    Added 10 stars

--- interstellar.js
+++ interstellar.js
@@ -28,52 +28,62 @@ Game.interstellar = (function(){
 			star1: {
 				name: "Alpha Centuri",
 				distance: 4.3,
-				planets: 1
+				planets: 1,
+				tier: 1
 			},
 			star2: {
 				name: "Barnard's Star",
 				distance: 5.96,
-				planets: 0
+				planets: 0,
+				tier: 3
 			},
 			star3: {
 				name: "Epsilon Eridani",
 				distance: 10.52,
-				planets: 2
+				planets: 2,
+				tier: 2
 			},
 			star4: {
 				name: "Tau Ceti",
 				distance: 11.89,
-				planets: 5
+				planets: 5,
+				tier: 2
 			},
 			star5: {
 				name: "Gliese 674",
 				distance: 14.81,
-				planets: 1
+				planets: 1,
+				tier: 4
 			},
 			star6: {
 				name: "Gliese 876",
 				distance: 15.34,
-				planets: 4
+				planets: 4,
+				tier: 4
 			},
 			star7: {
 				name: "Gliese 832",
 				distance: 16.08,
-				planets: 1
+				planets: 1,
+				tier: 4
 			},
 			star8: {
 				name: "Sirius",
 				distance: 8.58,
-				planets: 0
+				planets: 0,
+				tier: 2
 			},
 			star9: {
 				name: "Groombridge 1618",
 				distance: 15.85,
-				planets: 0
+				planets: 0,
+				tier: 3
 			},
 			star10: {
 				name: "DEN 0255-4700",
 				distance: 16.2,
-				planets: 0
+				planets: 0,
+				tier: 3
 			},
 		},
 	};
