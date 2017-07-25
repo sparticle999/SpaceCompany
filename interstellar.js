639fc99b509a8d8c0b0fb92da2e0491f2e38edcb
Sparticle999 <alexscruton@gmail.com>
2017-07-25 09:18:43 +0100

    Fixed Interstellar Backwards Compatibility

--- interstellar.js
+++ interstellar.js
@@ -167,16 +167,17 @@ Game.interstellar = (function(){
                     this.entries[i] = data.interstellar.stars[id];
                 }
             }
+            if(data.interstellar.interRocketBuilt && data.interstellar.interRocketBuilt === true){
+		        for(var i = 0; i < document.getElementsByClassName("interRocketBuild").length; i++){
+					document.getElementsByClassName("interRocketBuild")[i].className = "interRocketBuild hidden";
+				}
+				//document.getElementById("T1Rocket").className = "";
+				this.interRocketBuilt = true;
+				document.getElementById("interRocketBuilt").className = "green";
+				document.getElementById("interRocketBuilt").innerHTML = "Built";
+		    }
         }
-        if(data.interstellar.interRocketBuilt === true){
-	        for(var i = 0; i < document.getElementsByClassName("interRocketBuild").length; i++){
-				document.getElementsByClassName("interRocketBuild")[i].className = "interRocketBuild hidden";
-			}
-			//document.getElementById("T1Rocket").className = "";
-			this.interRocketBuilt = true;
-			document.getElementById("interRocketBuilt").className = "green";
-			document.getElementById("interRocketBuilt").innerHTML = "Built";
-	    }
+        
 	    this.refreshUI();
 	}
 
