10f0ee512961f6cc684df410c10ddd790ae8a09e
Sparticle999 <alexscruton@gmail.com>
2017-09-27 06:37:15 +0100

    Update stargazeData.js

--- data/stargazeData.js
+++ data/stargazeData.js
@@ -140,7 +140,7 @@ Game.prestigeData = (function(){
 
 	instance.rebirth = {
 		name: "Rebirth",
-		desc: "Stepping forth into a new life is a great undertaking and not something to be done on a whim. Once certain, you may start afresh, maintaining the knowledge and experience you gave gained from your previous life and renew yourself, achieving greater and faster than before. You will keep any unspent dark matter, as well as your upgrades. <br><b>NB: You cannot rebirth without a sphere, even on second runs.<br> NB: You will lose all upgrades purchased in your last life, so make sure you have more dark matter than you did last time.</b>",
+		desc: "Stepping forth into a new life is a great undertaking and not something to be done on a whim. Once certain, you may start afresh, maintaining the knowledge and experience you gave gained from your previous life and renew yourself, achieving greater and faster than before. You will keep any unspent dark matter, as well as your upgrades. <br><b>NB: You cannot rebirth without a sphere, even on second runs.<br> NB: You will keep all upgrades purchased in your previous life  </b>",
 		cost: 0,
 		category: "intro",
 	};
