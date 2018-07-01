5d90df5d59435aae2cec00725122809faa1c1732
Katorone <pbarandat@gmail.com>
2018-07-01 23:38:13 +0200

    Added the function addManualResource(id) to take care of the resources gained when pressing the gain buttons.

--- resources.js
+++ resources.js
@@ -44,8 +44,39 @@ function gainResources(delta) {
 	antimatter += antimatterps * delta;
 }
 
-// Gain Buttons
+function addManualResource(id) {
+    // Known resource?
+    var Obj = Game.resources.entries;
+    if (!contains(Object.keys(Obj), id)) { return false; }
+    // Resource doesn't allow manual gain, or is hidden.
+    if (!Obj[id].manualgain ||Obj[id].hidden) { return false; }
+    // Resource has full storage
+    var current = Obj[id].current;
+    var capacity = Obj[id].capacity;
+    if (current >= capacity) {return false;}
+    // Find the input for this resource.
+    if (!contains(Object.keys(Obj[id].buildings), id+'T1')) {
+        console.log("Couldn't find the building '"+id+"T1'.") ; return false;
+    }
+    var gainNum = Obj[id].gainNum;
+    var transaction = Obj[id].buildings[id+'T1'].resourcePerSecond
+    // Can we afford the cost?
+    var affordable = Object.keys(transaction).every(
+        res => transaction[res] > 0 || Obj[res].current+(transaction[res]*gainNum) >= 0
+    )
+    if (!affordable) {return false;}
+    // Add & remove all the resources in resourcePerSecond
+    // This will still take all the costs, even if storage overflows
+    Object.keys(transaction).forEach(
+        res => (transaction[res] > 0) ? 
+        		Game.resources.addResource(res, gainNum, true) : 
+        		Game.resources.takeResource(res, Math.abs(transaction[res]*gainNum))
+    )
+};
 
+
+// Gain Buttons - REPLACED BY addManualResource(id), taking the T1 input as cost
+/*
 function gainResource(resource){
 	if(resource === RESOURCE.Plasma){
 		if(getResource(RESOURCE.Energy) >= 1000 * gainNum && getResource(RESOURCE.Hydrogen) >= 10 * gainNum && getResource(RESOURCE.Plasma) < getMaxPlasma()){
@@ -71,7 +102,7 @@ function gainResource(resource){
 		}
 	}
 }
-
+*/
 function toggleCharcoal(){
 	charcoalToggled = !charcoalToggled;
 }
