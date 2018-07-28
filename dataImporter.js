f5bdb7a6460b38f7df1303dcd6083cc84274ec87
Miguel Saiz <miguelsaizo@gmail.com>
2018-07-28 09:35:43 -0400

    Expands documentation with complexity info + simplifies API

--- dataImporter.js
+++ dataImporter.js
@@ -1,17 +1,35 @@
 Game.data.import = (function () {
+    const didImportData = { };
+
     /**
-     * Merges data with the associated templates. Expects templates to be called `key` + `Template`. Will only check the first level of the objects
-     * For example, a template for the `storage` data would be called `storageTemplate` and would be applied to all object in `storage`
-     * @param {any} data Arbitrary data
+     * Internal flag for developers, this will warn when reimporting some data twice. Has no functional side effects
+     * @param {string} dataName The name of the data
+     * @complexity O(1)
+     */
+    function setDidImport(dataName) {
+        if (didImportData[dataName]) {
+            console.warn(`Imported ${dataName} twice, this should be avoided because it can potentially be a huge performance cost`);
+        }
+        didImportData[dataName] = true;
+    }
+
+    /**
+     * Merges data with the associated templates. Expects templates to be called `key` + `Template`. If no Templates are specified, will just return the entry. Will only check the first level of the objects
+     * @example a template for the `storage` data would be named `storageTemplate` and would be applied to all object in `storage`
+     * @param {object} data Arbitrary data
+     * @returns A copy of the modified data (with all templates applied)
+     * @complexity O(n * m) where n = number of templates and m = number of subobject affected by the template
      */
     function mergeTemplates(data) {
         // Get the templates to modify
         let templates = Object.keys(data)
             .filter(k => k.endsWith('Template'));
+            //n
 
         // Get the non-template items
         let items = Object.keys(data)
             .filter(k => !k.endsWith('Template'));
+            //n
 
         // Copy over all non-templates to the result
         let nonTemplates = items.reduce((result, current) => {
@@ -37,13 +55,16 @@ Game.data.import = (function () {
 
     /**
      * Applies a transformation to all values inside an object
-     * @param {*} target The object to transform
-     * @param {(object, string): void} transformer The anonymous function used to transform the data
+     * @param {object} target The object to transform
+     * @callback {(object, string): void} transformer The anonymous function used to transform the data
+     * @returns A copy of target with all templates applied
+     * @complexity O(n) where n = keys in object
      */
     function mapObjects(target, transformer) {
         const keys = Object.keys(target);
         return keys.reduce((result, current) => {
-            result[current] = transformer(target[current], current);
+            const transformation = transformer(target[current], current)
+            result[current] = $.extend({}, target[current], transformation);
             return result;
         }, {});
     }
@@ -52,6 +73,7 @@ Game.data.import = (function () {
      * Imports all resource data
      */
     function importResourceData() {
+        setDidImport('resource');
 
         /** Import the data and merge static templates **/
         const data = mergeTemplates(Game.data.resources);
@@ -59,7 +81,7 @@ Game.data.import = (function () {
         /** Merge dynamic templates **/
 
         data.storage = mapObjects(data.storage, (item, key) => {
-            return $.extend({}, item, {
+            return {
                 id: key,
                 htmlId: "store_" + key,
                 apply: function (self) {
@@ -71,20 +93,20 @@ Game.data.import = (function () {
                     res.displayNeedsUpdate = true;
                     self.displayNeedsUpdate = true;
                 },
-            });
+            };
         })
 
         data.items = mapObjects(data.items, (item, key) => {
-            return $.extend({}, item, {
+            return {
                 id: key,
                 resource: key,
                 htmlId: 'res_' + key,
                 capacity: item.baseCapacity,
-            });
+            };
         });
 
         data.categories = mapObjects(data.categories, (item, key) => {
-            return $.extend({}, item, { id: key });
+            return { id: key };
         });
 
         return data;
