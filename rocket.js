6161e780c383fcfb1e5423d7f8f4f2faf788ce4b
sparticle999 <alexscruton@gmail.com>
2020-01-27 21:14:59 +0000

    Hotfix

--- rocket.js
+++ rocket.js
@@ -14,7 +14,7 @@ Game.interstellar.rocket = (function(){
                 id: id,
                 htmlId: 'roc_' + id,
                 count: 0,
-                built: false;
+                built: false,
                 displayNeedsUpdate: true
             });
         }
