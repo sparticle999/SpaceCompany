e7f90eb577c668139529e97f53a9c749d667ac0f
Ian Golsby <iangolsby49@gmail.com>
2020-08-25 13:24:07 -0500

    Left my mark

--- data/splashTextData.js
+++ data/splashTextData.js
@@ -105,6 +105,7 @@ var splashTextArray = ["Venturing Into The Unknown...",
 "Recruiting Robot Hamsters...",
 "Autoclickers strictly discouraged!",
 "Even more in-depth than Kerbal Space Program!",
+"Effects of prolonged exposure to Space Company currently unknown...",
 ];
 var splashText = splashTextArray[Math.floor(Math.random()*splashTextArray.length)];
 
